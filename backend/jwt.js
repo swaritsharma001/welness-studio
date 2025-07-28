50fabb40f53a336b75019d16c0ff2e2576d3b4d3
UJJWAL SHARMA <152766614+swaritsharma001@users.noreply.github.com>
2025-07-28 11:52:43 +0000

    Bs

--- backend/jwt.js
+++ backend/jwt.js
@@ -16,8 +16,8 @@ export async function Createtoken(user){
 
 // Middleware to verify token
 export async function verifyToken(req, res, next){
-  const token = req.headers.authorization
-  console.log(token)
+  const authHeader = req.headers.authorization;
+const token = authHeader && authHeader.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Please login first -- no token" });
 
   try {
